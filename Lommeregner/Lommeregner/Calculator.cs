6307de846a074558ce466713f20402c78ca1381b
Mathias Nielsen <mgni62378@edu.ucl.dk>
2024-02-29 14:35:48 +0100

    Lavet lommeregner script

---
+++ Lommeregner/Lommeregner/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lommeregner
+{
+    internal class Calculator
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static double Divide(int num1, int num2)
+        {
+            return Convert.ToDouble(num1) / Convert.ToDouble(num2);// vi kan regne med double, hvis vi laver dem til double, hvis de er int.
+            
+        }
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+
+        }
+
+ 
+
+    }
+
+}
+//private inden for sit eget program
+//public uden for sit eget program
+
+
+//Benyt boksen i toppen af side 4 i opgaven!! 
+
---
+++ Lommeregner/Lommeregner/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace Lommeregner
+{
+
+
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            bool køerneKommerHjem = false;
+            while (køerneKommerHjem == false)
+            {
+                Console.Clear();
+                int num3;
+                double num4;
+                string stop;
+
+                Console.WriteLine("Når du vil stoppe, skriv stop");
+                Console.Write("Indtast et nummer: ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.Write("Indtast et regnetegn: ");
+                string symbol = Console.ReadLine();
+
+                Console.Write("Indtast et nummer: ");
+                int num2 = int.Parse(Console.ReadLine());
+
+
+
+                if (symbol == "+")
+                {
+
+                    num3 = Calculator.Add(num1, num2);
+
+                    Console.Write("Dit Resultat er: " + num3);
+
+                }
+                else if (symbol == "-")
+                {
+                    num3 = Calculator.Subtract(num1, num2);
+
+                    Console.WriteLine("Dit Resultat er: " + num3);
+
+                }
+                else if (symbol == "/")
+                {
+
+                    num4 = Calculator.Divide(num1, num2);
+
+                    Console.WriteLine("Dit Resultat er: " + num4);
+
+                }
+                else if (symbol == "*")
+
+                {
+                    num3 = Calculator.Multiply(num1, num2);
+
+                    Console.WriteLine("Dit Resultat er: " + num3);
+
+                }
+                else
+                {
+                    Console.WriteLine("Din indtastning er forkert");
+                }
+
+                Console.WriteLine("\nReset point");
+                stop = Console.ReadLine();
+
+                if (stop == "stop")
+                {
+                    Console.WriteLine("Tak for denne gang");
+                    køerneKommerHjem = true;
+
+                }
+                    
+
+            }
+        }
+
+    }
+}
+//
+//Udfyld med jeres egne værdier/navne
+//ClassNavn.MetodeNavn()        //eksempel!!!
+
+  /*class Car
+    {
+        public int Speed(int engineRevolutions, int engineGear)
+        {
+            return (engineRevolutions / 100) * (engineGear ^ 1.08);
+        }
+        public string model = "Mustang";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.model);
+        }
+    }*/
